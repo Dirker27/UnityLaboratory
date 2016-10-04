34192c2f815937b70fe969840575ee55baf68499
Dirk <hortensd@gmail.com>
2016-10-03 22:17:17 -0700

    Views -> Hierarchy and Console

---
+++ UnityLaboratory/Assets/Scripts/LogMessages.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogMessages : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello, World.");
+        Debug.Log("You sexy devil.");
+
+        Debug.LogWarning("The end is nigh.");
+
+        Debug.LogError("And we're out of TP.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
